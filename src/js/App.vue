29079e4c16d5d84ece380730b834c036207d7a4d
Anti Christ <fordznotmatter@gmail.com>
2023-07-12 22:26:21 -0700

    deleted all
    deleted all

---
+++ src/js/App.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="wrapper">
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;600;800&display=swap');
+
+$maincolor: #000000;
+$textcolor: #fff;
+$additionalcolor: #25AB75;
+
+::selection {
+    background: $additionalcolor;
+}
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+body {
+    background-color: $maincolor;
+    color: $textcolor;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+    font-size: 1em;
+}
+
+a {
+    color: #ababab;
+    text-decoration: none;
+    transition: color 200ms ease;
+
+    &:hover {
+        color: $textcolor;
+    }
+}
+
+.wrapper {
+    overflow: hidden;
+}
+
+.container {
+
+    @media(min-width: 1500px) {
+        width: 1300px;
+        margin: 0 auto;
+    }
+    @media(max-width: 1500px) and (min-width: 900px) {
+        width: 80%;
+        margin: 0 10%;
+    }
+    @media(max-width: 900px) {
+        width: 90%;
+        margin: 0 5%;
+    }
+}
+
+</style>
---
+++ src/js/components/tarrifs.vue
@@ -0,0 +1,112 @@
+<template>
+    <div>
+        <h3>Тарифы</h3>
+        <div class="tarrifs">
+            <div>
+                <h4>Free</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Quis nostrud exercitation ullamco</li>
+                    <li>Exercitation ullamco</li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Excepteur sint occaecat excepteur sint occaecat</li>
+                    <li>Sed ut perspiciatis unde omnis</li>
+                </ul>
+                <span><a href="#">More</a>  <i class="fa-solid fa-arrow-right"></i></span>
+            </div>
+            <div>
+                <h4>Plus+</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <ul>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Quis nostrud exercitation ullamco</li>
+                    <li>Exercitation ullamco</li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                    <li>Excepteur sint occaecat excepteur sint occaecat</li>
+                    <li>Sed ut perspiciatis unde omnis</li>
+                    <li>Exercitation ullamco</li>
+                    <li>Lorem ipsum dolor sit amet</li>
+                </ul>
+                <button>Join us</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+h3 {
+    margin: 70px 0;
+    font-size: 3em;
+    font-weight: 400;
+}
+
+.tarrifs {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-bottom: 150px;
+
+    >div {
+        @media (min-width: 750px){width: 35%}
+        background-color: #232532;
+        color: #A8A7A8;
+        padding: 50px 5%;
+
+        &:last-of-type {
+            background-color: #323443;
+            color: #ADACAE;
+            @media (max-width: 750px){margin-top: 30px}
+        }
+
+        h4 {
+            color: white;
+            text-align: center;
+            font-size: 2em;
+            margin-bottom: 30px;
+        }
+
+        ul {
+            margin: 20px 30px;
+        }
+
+        span {
+
+            a {
+                color: #0070C9;
+                &:hover {
+                color: #41a9ff;
+                }
+            }
+
+            i {
+                margin-left: 9px;
+                font-size: 1.2em;
+            }
+        }
+
+        button {
+            background-color: #0070C9;
+            border: none;
+            border-radius: 10px;
+            color: white;
+            padding: 5px 10px;
+            cursor: pointer;
+            transition: all 400ms ease;
+
+            &:hover {
+                background-color: #41a9ff;
+                transform: scale(1.1);
+                color: black;
+            }
+        }
+    }
+}
+    
+</style>
